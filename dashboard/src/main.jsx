9c48084222e138d6e2b15466b949159ec225395d
Carlos-Candela <karlo0310@gmail.com>
2025-04-06 14:06:02 +0200

    Loading Icon

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -2,13 +2,16 @@ import { lazy, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom'; 
+import {Grid} from 'react-loading-icons'
+
 
 
 const App = lazy(() => import('./App'));
 
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <Suspense fallback={<div className='flex justify-center items-center min-h-screen bg-[#c9c4f4]'>Loading...</div>}>
+    <Suspense fallback={<div className='flex justify-center items-center min-h-screen bg-[#c9c4f4]'><Grid/></div>}>
       <App />
     </Suspense>
   </BrowserRouter>,
--- dashboard/src/views/auth/AdminLogin.jsx
+++ dashboard/src/views/auth/AdminLogin.jsx
@@ -1,81 +1,80 @@
 import React, { useState } from "react";
 import logo from "../../assets/sellurbike.png";
 
-
 const AdminLogin = () => {
-    const [formLoginData, setFormLoginData] = useState({
-        email: "",
-        password: "",
+  const [formLoginData, setFormLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const inputHandle = (e) => {
+    setFormLoginData({
+      ...formLoginData,
+      [e.target.name]: e.target.value,
     });
+  };
 
-    const inputHandle = (e) => {
-        setFormLoginData({
-            ...formLoginData,
-            [e.target.name]: e.target.value,
-        });
-    }
-
-    const submit = (e) => {
-        e.preventDefault();
-        console.log(formLoginData);
-    }
-    return (
-        <>
-            <div className="min-w-screen min-h-screen bg-[#c9c4f4] flex justify-center items-center">
-                <div className="w-[450px] text-[#ffffff] p-2">
-                    <div className="bg-[#6f68d1] p-4 rounded-md">
-                        <img src={logo} alt="SellURBike" className="mx-auto" />
-                        <form onSubmit={submit}>
-                            <div className="mb-4">
-                                <label
-                                    htmlFor="email"
-                                    className="block text-sm font-medium text-white"
-                                >
-                                    Correo Electrónico
-                                </label>
-                                <input
-                                    onChange={inputHandle}
-                                    value={formLoginData.email}
-                                    type="email"
-                                    name="email"
-                                    id="email"
-                                    className="w-full px-3 py-2 mt-1 
+  const submit = (e) => {
+    e.preventDefault();
+    console.log(formLoginData);
+  };
+  return (
+    <>
+      <div className="min-w-screen min-h-screen bg-[#c9c4f4] flex justify-center items-center">
+        <div className="w-[450px] text-[#ffffff] p-2">
+          <div className="bg-[#6f68d1] p-4 rounded-md">
+            <img src={logo} alt="SellURBike" className="mx-auto" />
+            <form onSubmit={submit}>
+              <div className="mb-4">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-white"
+                >
+                  Correo Electrónico
+                </label>
+                <input
+                  onChange={inputHandle}
+                  value={formLoginData.email}
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="w-full px-3 py-2 mt-1 
               text-gray-700 bg-gray-200 rounded-md focus:outline-none 
               focus:ring focus:ring-indigo-100 focus:bg-white"
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <label
-                                    htmlFor="password"
-                                    className="block text-sm font-medium text-white"
-                                >
-                                    Contraseña
-                                </label>
-                                <input
-                                    onChange={inputHandle}
-                                    value={formLoginData.password}
-                                    type="password"
-                                    name="password"
-                                    id="password"
-                                    className="w-full 
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-white"
+                >
+                  Contraseña
+                </label>
+                <input
+                  onChange={inputHandle}
+                  value={formLoginData.password}
+                  type="password"
+                  name="password"
+                  id="password"
+                  className="w-full 
                                         px-3 py-2 mt-1 text-gray-700 bg-gray-200 rounded-md 
                                         focus:outline-none focus:ring focus:ring-indigo-100 focus:bg-white"
-                                />
-                            </div>
-                            <button
-                                type="submit"
-                                className="w-full py-2 px-4 bg-[#161271] text-white 
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full py-2 px-4 bg-[#161271] text-white 
             font-bold rounded-md hover:bg-[#232342] focus:outline-none 
             focus:ring focus:ring-indigo-100 focus:bg-white cursor-pointer"
-                            >
-                                Iniciar Sesión
-                            </button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+              >
+                Iniciar Sesión
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AdminLogin;
