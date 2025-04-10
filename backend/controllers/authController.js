6e9bedcd413f97e269183251226acef554e6fbc8
Carlos-Candela <karlo0310@gmail.com>
2025-04-10 11:25:34 +0200

    messages admin login to spanish

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -10,19 +10,19 @@ class authControllers {
       const admin = await adminModel.findOne({ email }).select("+password");
       
       if (!admin) {
-        return responseReturn(res, 404, { error: "Admin not found" });
+        return responseReturn(res, 404, { error: "No es un administrador correcto" });
       }
 
       const match = await bcrypt.compare(password, admin.password);
 
       if (!match) {
-       return responseReturn(res, 401, { error: "Password not match" });
+       return responseReturn(res, 401, { error: "La contraseña no es correcta" });
       } else {
         const token = await createToken({ id: admin._id, role: admin.role });
         res.cookie("accesToken", token, {
           expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
         });
-        return responseReturn(res, 200, { token, message: "Login successfully" });
+        return responseReturn(res, 200, { token, message: "Login correcto" });
       }
     } catch (error) {
       return responseReturn(res, 500, { error: error.mesagge });
