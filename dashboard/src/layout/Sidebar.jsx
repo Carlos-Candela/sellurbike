a32dd77fa11cb3f7b9384c5f21ac27ffa565afae
Carlos-Candela <karlo0310@gmail.com>
2025-04-12 13:26:03 +0200

    All complete Sidebar

--- dashboard/src/layout/Sidebar.jsx
+++ dashboard/src/layout/Sidebar.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import logo from "../assets/sellurbike.png";
 import { getNav } from "../navigation/index.js";
+import { IoMdLogOut } from "react-icons/io";
+
 
 const Sidebar = () => {
   const { pathname } = useLocation();
@@ -34,15 +36,22 @@ const Sidebar = () => {
                   to={nav.path}
                   className={`${
                     pathname === nav.path
-                      ? "bg-blue-500 shadow-indigo-500/50 text-white duration-500 w-full"
+                      ? "bg-blue-400 shadow-indigo-500/50 text-white duration-500 w-full font-bold"
                       : "text-[#030811] duration-200"
-                  } px-[12px] py-[9px] flex justify-center items-center gap-2 p-2 rounded-md hover:bg-blue-200 transition-all hover:pl-4 transintion-all w-full mb-1`}
+                  } px-[12px] py-[9px] flex items-center gap-2 p-2 rounded-md hover:bg-blue-200 transition-all hover:pl-4 transintion-all w-full mb-1`}
                 >
                   <span >{nav.icon}</span>
                   <span >{nav.title}</span>
                 </Link>
               </li>
             ))}
+            <li>
+              <button className='text-[#030811] duration-200
+                  px-[12px] py-[9px] flex items-center gap-2 p-2 rounded-md hover:bg-blue-200 transition-all hover:pl-4 transintion-all w-full mb-1'>
+                    <span><IoMdLogOut/></span>
+                    <span>Cerrar Sesion</span>
+                  </button>
+            </li>
           </ul>
         </div>
       </div>
--- dashboard/src/navigation/allNav.jsx
+++ dashboard/src/navigation/allNav.jsx
@@ -1,12 +1,69 @@
 import { MdOutlineDashboard } from "react-icons/md";
+import { MdOutlineBorderColor } from "react-icons/md";
+import { MdOutlinePayments } from "react-icons/md";
+import { MdOutlineCategory } from "react-icons/md";
+import { LuUsersRound } from "react-icons/lu";
+import { FiUserMinus } from "react-icons/fi";
+import { RiUserShared2Line } from "react-icons/ri";
+import { IoChatboxEllipsesOutline } from "react-icons/io5";
+
 
 export const allNav = [
     {
         id:1,
-        title: "Dashboard",
+        title: "Panel de Control",
         icon: <MdOutlineDashboard />,
         role: "admin",
         path: "/admin/dashboard",
+    },
+    {
+        id:2,
+        title: "Pedidos",
+        icon: <MdOutlineBorderColor />,
+        role: "admin",
+        path: "/admin/dashboard/orders",
+    },
+    {
+        id:3,
+        title: "Categorias",
+        icon: <MdOutlineCategory />,
+        role: "admin",
+        path: "/admin/dashboard/category",
+    },
+    {
+        id:4,
+        title: "Vendedores",
+        icon: <LuUsersRound />,
+        role: "admin",
+        path: "/admin/dashboard/sellers",
+    },
+    {
+        id:5,
+        title: "Pagos",
+        icon: <MdOutlinePayments />,
+        role: "admin",
+        path: "/admin/dashboard/payments",
+    },
+    {
+        id:6,
+        title: "Desactivar Vendedores",
+        icon: <FiUserMinus />,
+        role: "admin",
+        path: "/admin/dashboard/deactive-sellers",
+    },
+    {
+        id:7,
+        title: "Solicitud Vendedores",
+        icon: <RiUserShared2Line />,
+        role: "admin",
+        path: "/admin/dashboard/sellers-request",
+    },
+    {
+        id:8,
+        title: "Live Chat",
+        icon: <IoChatboxEllipsesOutline />,
+        role: "admin",
+        path: "/admin/dashboard/chat-seller",
     },
     {
         id:1,
